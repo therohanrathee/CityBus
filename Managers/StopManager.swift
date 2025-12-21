3a6da9e950793b770d0e4c8926356ad919c637f8
Rohan Rathee <rohanrathee4185@gmail.com>
2025-12-21 13:43:53 +0530

    Updated Stops Location

--- Managers/StopManager.swift
+++ Managers/StopManager.swift
@@ -18,77 +18,77 @@ class StopManager {
     
     let rajeevNagarChowk = BusStop(
         name: "Rajeev Nagar chowk",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4660, longitude: 77.0370) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.47608, longitude: 77.03479)
     )
     
     let crpfCampChowk = BusStop(
         name: "CRPF Camp Chowk",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4680, longitude: 77.0390) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.47847, longitude: 77.03510)
     )
     
     let sheetlaMataA = BusStop(
         name: "Sheetla Mata Mandir (A)",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4700, longitude: 77.0410) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.47897, longitude: 77.03174)
     )
     
     let sheetlaMataB = BusStop(
         name: "Sheetla Mata Mandir (B)",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4705, longitude: 77.0415) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.47997, longitude: 77.02784)
     )
     
     let sector5Ramlila = BusStop(
         name: "Sector - 5 Ramlila Ground",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4730, longitude: 77.0430) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.48283, longitude: 28.48283)
     )
     
     let ashokViharPetrol = BusStop(
         name: "Ashok Vihar Phase 2 Petrol Pump",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4760, longitude: 77.0450) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.49046, longitude: 77.01967)
     )
     
     let ashokViharExt = BusStop(
         name: "Ashok Vihar Phase III Extension",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4800, longitude: 77.0480) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.49437, longitude: 77.02036)
     )
     
     let parkViewRes = BusStop(
         name: "Park View Residency",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4850, longitude: 77.0500) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.49754, longitude: 77.02277)
     )
     
     let palamBlockC1 = BusStop(
         name: "Palam Vihar Block C1/Ashok Vihar Phase III",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4900, longitude: 77.0520) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.50018, longitude: 77.02629) // TODO: UPDATE COORDINATE
     )
     
     let krishnaChowk = BusStop(
         name: "Krishna Chowk Palam Vihar",
-        coordinate: CLLocationCoordinate2D(latitude: 28.4950, longitude: 77.0550) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.50205, longitude: 77.02888) // TODO: UPDATE COORDINATE
     )
     
     let dharamColony = BusStop(
         name: "Dharam Colony",
-        coordinate: CLLocationCoordinate2D(latitude: 28.5000, longitude: 77.0580) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.50354, longitude: 77.03549) // TODO: UPDATE COORDINATE
     )
     
     let carterpuriVillage = BusStop(
         name: "Carterpuri Village",
-        coordinate: CLLocationCoordinate2D(latitude: 28.5050, longitude: 77.0600) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.50728, longitude: 77.03933) // TODO: UPDATE COORDINATE
     )
     
     let columbiaAsia = BusStop(
         name: "Columbia Asia Hospital",
-        coordinate: CLLocationCoordinate2D(latitude: 28.5100, longitude: 77.0620) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.50935, longitude: 77.04104) // TODO: UPDATE COORDINATE
     )
     
     let cosmosApt = BusStop(
         name: "Cosmos Executive Apartment",
-        coordinate: CLLocationCoordinate2D(latitude: 28.5150, longitude: 77.0650) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.51393, longitude: 77.03598) // TODO: UPDATE COORDINATE
     )
     
     let ramMandir = BusStop(
         name: "Ram Mandir Palam Vihar",
-        coordinate: CLLocationCoordinate2D(latitude: 28.5200, longitude: 77.0700) // TODO: UPDATE COORDINATE
+        coordinate: CLLocationCoordinate2D(latitude: 28.51501, longitude: 77.03449) // TODO: UPDATE COORDINATE
     )
     
     // --- Helper to get all stops as a list ---
